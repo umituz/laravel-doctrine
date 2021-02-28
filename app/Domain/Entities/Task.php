c3f3623d14a0628d0280c05092e7bc0f58a1903f
Ümit UZ <umituz998@gmail.com>
2021-02-28 15:49:30 +0300

    laravel doctrine example

---
+++ app/Domain/Entities/Task.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+use LaravelDoctrine\Extensions\Timestamps\Timestamps;
+
+/**
+ * @ORM\Entity()
+ */
+class Task
+{
+    use Timestamps;
+
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
